56a310c5607b583c66a852b1994cff8c144f6e4c
Vasyl <w.kyrychuk@gmail.com>
2018-08-29 01:02:35 +0300

    Homework2

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -6,32 +6,51 @@ using System.Threading.Tasks;
 
 namespace Homework2
 {
-    enum TestCaseStatus { Pass, Fail, Blocked, WP, Unexecuted };
+    enum HTTPError { BadRequest = 400, Unauthorized, PaymentRequired, Forbidden, NotFound, MethodNotAllowed }
 
-    struct RGB
+    struct Dog
     {
-        public byte red;
-        public byte green;
-        public byte blue;
+        public string name;
+        public string mark;
+        public int age;
+
+        public override string ToString()
+        {
+            return String.Format("Name {0}\nMark {1}\nAge {2}", name, mark, age);
+        }
     }
     class Program
     {
         
         static void Main(string[] args)
         {
-            TestCaseStatus test1Status = TestCaseStatus.Pass;
-            Console.WriteLine(test1Status);
-            
-            RGB white, black;
-            white.red = 255;
-            white.green = 255;
-            white.blue = 255;
-            black.red = 255;
-            black.green = 255;
-            black.blue = 255;
+            float a, b, c;
+            Console.WriteLine("Enter 3 float numbers");
+            float.TryParse(Console.ReadLine(), out a);
+            float.TryParse(Console.ReadLine(), out b);
+            float.TryParse(Console.ReadLine(), out c);
+            Console.WriteLine("Are they all belong to the range [-5,5]? \n{0}", (a >= -5 &&
+                a <= 5) ? (b >= -5 && b <= 5) ? (c >= -5 && c <= 5) ? "true" : "false" : "false" : "false");
+
+            int m, n, v;
+            Console.WriteLine("Enter 3 integer numbers");
+            Int32.TryParse(Console.ReadLine(), out m);
+            Int32.TryParse(Console.ReadLine(), out n);
+            Int32.TryParse(Console.ReadLine(), out v);
+            Console.WriteLine("Max {0}", (m > n) ? (m > v) ? m : v : (n > v) ? n : v);
+            Console.WriteLine("Min {0}", (m < n) ? (m < v) ? m : v : (n < v) ? n : v);
+
+            Console.WriteLine("Enter number of HTTP error");
+            Console.WriteLine(((HTTPError)Int32.Parse(Console.ReadLine())).ToString());
+
+            Dog myDog;
+            Console.WriteLine("Enter name, mark and age of your dog");
+            myDog.name = Console.ReadLine();
+            myDog.mark = Console.ReadLine();
+            Int32.TryParse(Console.ReadLine(), out myDog.age);
+            Console.WriteLine(myDog.ToString());
 
             Console.ReadKey();
-
         }
     }
 }
