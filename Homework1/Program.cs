753ea014038e936eb4378480164d6397e88504df
Vasyl <w.kyrychuk@gmail.com>
2018-08-28 22:58:52 +0300

    Add project files.

---
+++ Homework1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter two integer numbers");
+            Int32.TryParse(Console.ReadLine(), out a);
+            Int32.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("a+b = {0}\na-b = {1}\na*b = {2}\na/b = {3}", a + b, a - b, a * b, a / b);
+
+            Console.ReadKey();
+
+        }
+    }
+}
