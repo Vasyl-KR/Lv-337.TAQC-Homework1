fe245b9c54d1d668be7804dbdff83aa39c735ef4
Киричук Василь <w.kyrychuk@gmail.com>
2018-08-29 15:34:06 +0300

    y

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -12,28 +12,14 @@ namespace Homework2
         static void Main(string[] args)
         {
 
-            int day, month;
-            Console.WriteLine("Enter a day");
-            Int32.TryParse(Console.ReadLine(), out day);
-            Console.WriteLine("Enter a month");
-            Int32.TryParse(Console.ReadLine(), out month);
-            bool check = day > 0 && day <= 31 && month > 0 && month <= 12;
-            Console.WriteLine(check);
+            int a, b;
+            Console.WriteLine("Enter 2 integer numbers");
+            Int32.TryParse(Console.ReadLine(), out a);
+            Int32.TryParse(Console.ReadLine(), out b);
+            for (int i = a; i <= b ; i++)
+            {
 
-            double number;
-            Console.WriteLine("Enter a double number");
-            Double.TryParse(Console.ReadLine(), out number);
-            number *= 10;
-            double a = (int)number % 10;
-            number *= 10;
-            double b = (int)number % 100 % 10;
-            Console.WriteLine("First digit {0}, second digit {1}, sum {2}", a, b, a + b);
-
-            int h;
-            Console.WriteLine("Enter a hour");
-            Int32.TryParse(Console.ReadLine(), out h);
-            Console.WriteLine(h >= 0 ? h >= 6 ? h >= 12 ? h >= 18 ? h >= 24 ? "Too large number" : "Доброго вечора!" :
-                "Доброго дня!" : "Доброго ранку!" : "Доброї ночi!" : "Can't be negative");
+            }
 
             Console.ReadKey();
 
