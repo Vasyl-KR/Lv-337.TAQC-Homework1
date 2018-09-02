81f8d78d3850a6cbbec2bef47ffcd13e9718ea3e
Vasyl <w.kyrychuk@gmail.com>
2018-09-02 12:43:43 +0300

    Comment

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*a) Read the text as a string value and calculate the counts of characters 'a', 'o', 'i', 'e' in this text.
+  b) Ask user to enter the number of month. Read the value and write the amount of days in this month.
+  c) Enter 10 integer numbers. Calculate the sum of first 5 elements 
+  if they are positive or product of last 5 element in  the other case.*/
+
 namespace Homework3
 {
     class Program
