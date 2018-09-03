de8504ac8188b81156c94438fbc889d1c1e503cd
Vasyl <w.kyrychuk@gmail.com>
2018-09-03 15:57:23 +0300

    Task5-1

---
+++ Homework1/Bird.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class Bird : IFlyable
+    {
+        private string name;
+        private bool canFly;
+
+        public Bird(string name, bool canFly)
+        {
+            this.name = name;
+            this.canFly = canFly;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine(canFly);
+        }
+    }
+}
--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,29 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework1
+/*Develop interface IFlyable with method Fly().
+Create two classes Bird (with fields: name and canFly) and Plane  (with fields: mark and highFly),
+which implement interface IFlyable.
+Create List of IFlyable objects and add some Birds and Planes to it. 
+Call Fly() method for every item from the list of it*/
+
+namespace Homework5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a;
-            Console.WriteLine("Enter a integer number a");
-            Int32.TryParse(Console.ReadLine(), out a);
-            Console.WriteLine("Perimeter = {0}\t Area = {1}", 4 * a, a * a);
-
-            int age;
-            string name;
-            Console.WriteLine("What is your name?");
-            name = Console.ReadLine();
-            Console.WriteLine("How old are you, {0}?", name);
-            Int32.TryParse(Console.ReadLine(), out age);
-            Console.WriteLine("Name - {0}, Age - {1}", name, age);
-
-            double r;
-            Console.WriteLine("Enter double number r");
-            Double.TryParse(Console.ReadLine(), out r);
-            Console.WriteLine("Length - {0:F3}, Area - {1:F3}, Volume - {2:F3}", 2 * Math.PI * r, Math.PI * r * r, 4 / 3 * Math.PI * r * r * r);
+            List<IFlyable> flyes = new List<IFlyable>(3);
+            flyes.Add(new Bird("Owl", true));
+            flyes.Add(new Plane("Boing", 11000));
+            foreach (IFlyable f in flyes)
+            {
+                f.Fly();
+            }
 
             Console.ReadKey();
 
