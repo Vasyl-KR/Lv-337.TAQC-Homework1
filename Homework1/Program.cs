0bb4c7eec3a4e020286ef6039900ffa1ed5dad6c
Vasyl <w.kyrychuk@gmail.com>
2018-09-03 18:21:05 +0300

    Demo

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,64 +1,72 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*Create Console Application project in VS.
-   In method Main() write code for solving next task:
-        a) read 3 float numbers and check: are they all belong to the range [-5,5].
-        b) read 3 integers and write max and min of them.
-        c) read number of HTTP Error (400, 401,402, ...) and write the name of this error (Declare enum HTTPError)
-        d) declare struct Dog with fields Name, Mark, Age. Declare variable myDog of Dog type and read values for it. 
-        Output myDos into console. (Declare method ToString in struct)
-*/
-namespace Homework2
+namespace Demo
 {
-    enum HTTPError { BadRequest = 400, Unauthorized, PaymentRequired, Forbidden, NotFound, MethodNotAllowed }
-
-    struct Dog
-    {
-        public string name;
-        public string mark;
-        public int age;
-
-        public override string ToString()
-        {
-            return String.Format("Name {0}\nMark {1}\nAge {2}", name, mark, age);
-        }
-    }
     class Program
     {
         
         static void Main(string[] args)
         {
-            //task1
-            float a, b, c;
-            Console.WriteLine("Enter 3 float numbers");
-            float.TryParse(Console.ReadLine(), out a);
-            float.TryParse(Console.ReadLine(), out b);
-            float.TryParse(Console.ReadLine(), out c);
-            Console.WriteLine("Are they all belong to the range [-5,5]? \n{0}", (a >= -5 &&
-                a <= 5) ? (b >= -5 && b <= 5) ? (c >= -5 && c <= 5) ? "true" : "false" : "false" : "false");
-            //task2
-            int m, n, v;
-            Console.WriteLine("Enter 3 integer numbers");
-            Int32.TryParse(Console.ReadLine(), out m);
-            Int32.TryParse(Console.ReadLine(), out n);
-            Int32.TryParse(Console.ReadLine(), out v);
-            Console.WriteLine("Max {0}", (m > n) ? (m > v) ? m : v : (n > v) ? n : v);
-            Console.WriteLine("Min {0}", (m < n) ? (m < v) ? m : v : (n < v) ? n : v);
-            //task3
-            Console.WriteLine("Enter number of HTTP error");
-            Console.WriteLine(((HTTPError)Int32.Parse(Console.ReadLine())).ToString());
-            //task4
-            Dog myDog;
-            Console.WriteLine("Enter name, mark and age of your dog");
-            myDog.name = Console.ReadLine();
-            myDog.mark = Console.ReadLine();
-            Int32.TryParse(Console.ReadLine(), out myDog.age);
-            Console.WriteLine(myDog.ToString());
+            int[] nums = new int[4];
+            int[] nums2 = new int[4] { 1, 2, 3, 5 };
+            int[] nums5 = { 1, 2, 3, 5 };
 
+            int[] nums = new int[4];
+            nums[0] = 1;
+            nums[1] = 2;
+            nums[2] = 3;
+            nums[3] = 5;
+            Console.WriteLine(nums[3]);  // 5
+            nums[4] = 9; //IndexOutOfRangeException
+
+            int[] nums1 = new int[] { 0, 1, 2, 3, 4, 5 };
+
+            int[,] nums2 = { { 0, 1, 2 }, { 3, 4, 5 } };
+
+            int[,] nums1;
+            int[,] nums2 = new int[2, 3];
+            int[,] nums3 = new int[2, 3] { { 0, 1, 2 }, { 3, 4, 5 } };
+            int[,] nums4 = new int[,] { { 0, 1, 2 }, { 3, 4, 5 } };
+            int[,] nums5 = new[,] { { 0, 1, 2 }, { 3, 4, 5 } };
+            int[,] nums6 = { { 0, 1, 2 }, { 3, 4, 5 } };
+
+            int[,] mas = { { 1, 2, 3 },
+                           { 4, 5, 6 },
+                           { 7, 8, 9 },
+                           { 10, 11, 12 } };
+            foreach (int i in mas)
+                Console.Write($"{i} ");
+
+
+            ArrayList list = new ArrayList();
+            list.Add(2.3); // заносимо в список об'єкт типу double
+            list.Add(55); // заносимо в список об'єкт типу int
+            list.AddRange(new string[] { "Hello", "world" }); // заносимо в список масив рядків
+
+            // перебір значень
+            foreach (object o in list)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine();
+            // видаляєм перший елемент
+            list.RemoveAt(0);
+            // перевертаєм список
+            list.Reverse();
+            // отримання елементу за індексом
+            Console.WriteLine(list[0]);
+            Console.WriteLine();
+            // властивість Count
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            
             Console.ReadKey();
         }
     }
