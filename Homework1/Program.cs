e83d3294494bfbfa6be430d753dc772276a14969
Vasyl <w.kyrychuk@gmail.com>
2018-09-09 23:47:27 +0300

    C

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+   1) Create abstract class Shape with field name and property Name. 
+   Add constructor with 1 parameter  and abstract methods Area() and Perimeter(), 
+   which can return area and perimeter of shape; 
+   Create classes Circle, Square derived from Shape with field radius (for Circle) and side (for Square).   
+   Add necessary constructors, properties to these classes, override methods from abstract class Shape. 
+   a) In Main() create list of Shape, then ask user to enter data of 10 different shapes.  
+   Write name, area and perimeter of all shapes. 
+   b) Find shape with the largest perimeter and print its name. 
+   3) Sort shapes by area and print obtained list (Remember about IComparable)
+ */
+
 namespace Homework7
 {
     class Program
