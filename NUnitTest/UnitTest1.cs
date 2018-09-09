b12ce208df43ecca9a3d9913fd59805e7564f5c5
Vasyl <w.kyrychuk@gmail.com>
2018-09-10 01:49:09 +0300

    ok

---
+++ NUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Test;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Calculator calc = new Calculator();
+            int result = 15;
+            int expectedResult = calc.Add(7, 8);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
