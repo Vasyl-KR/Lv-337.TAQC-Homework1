0d586c4b3198ce239b53983cb916184aeae1db5b
Vasyl <w.kyrychuk@gmail.com>
2018-09-09 23:46:05 +0300

    C

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+1) Додайте два класи Person та Staff (використайте код з презентації)
+2) Утворіть два класи Teacher та  Developer, похідні від Staff. Для класу Teaсher додайте поле subject, 
+а для класу Developer – поле level, перевизначте метод Print для обох класів.
+3)В Main  визначте список типу Person та додайте до нього об’єкти кожного типу.
+Викличте для кожного елементу списку метод Print()
+4) Введіть ім’я персони і знайдіть чи є така персона у вашому списку, якщо так – видрукуйте про неї інформацію
+*/
+
 namespace Homework7
 {
     class Program
