a727fda5dbaad451f32e77f112e655ccdf3ef508
Vasyl <w.kyrychuk@gmail.com>
2018-09-09 21:11:46 +0300

    Task6

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,83 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*  Утворити і заповнити колекцію з 10 цілих чисел
-    Отримати і вивести на консоль тільки від’ємні числа
-    Отримати і вивести на консоль тільки додатні парні числа
-    Отримати найбільший та найменший елементи з масиву, а також суму всіх елементів.
-    Отримати перший елемент з масиву, який є менший ніж середнє арифметичне значення елементів масиву
-    Посортувати масив з використанням OrderBy*/
+/*  Create method Div(), which calculates the dividing of two  double numbers. 
+    In Main input  2 double numbers and call this method. Catch appropriative exceptions. 
+
+    Read and write all data in task1 from data.txt into rez.txt files, by using (Catch appropriative exceptions):
+        1. StreamReader StreamWriter
+        2. File.WriteAllText
+    Write into file ‘DirectoryC.txt’ information (name, type, size) 
+    about all directories and files from disc C on your computer. Catch appropriative exceptions.
+
+    Select from directory D only .txt files and print the text from it into console. 
+    Catch appropriative exceptions.
+*/
 
 
-namespace Homework8
+namespace Homework6
 {
     class Program
     {
-        
-        static void Main(string[] args)
+
+        static void Main()
         {
-            //task 1
-            List<int> numbers = new List<int>();
-            Random rand = new Random();
-            for (int i = 0; i < 10; i++)
+            //task1
+            double a, b;
+            try
             {
-                numbers.Add(rand.Next(-100, 100));
+                Console.WriteLine("Enter two numbers");
+                a = Double.Parse(Console.ReadLine());
+                b = Double.Parse(Console.ReadLine());
+                Console.WriteLine("{0} / {1} = {2}", a, b, DoubleResult(a, b));
             }
-            foreach (int i in numbers)
+            catch (FormatException)
             {
-                Console.Write(i + " ");
+                Console.WriteLine("Invalid double number");
             }
-            Console.WriteLine();
-
-            //task 2
-            var negative = from i in numbers
-                           where i < 0
-                           select i;
-
-            Console.WriteLine("Negative");
-            foreach (var i in negative)
+            catch (DivideByZeroException)
             {
-                Console.Write(i + " ");
+                Console.WriteLine("Dividing by zero is not allowed");
             }
-            Console.WriteLine();
 
-            //task 3
-            var positiveEqual = from i in numbers
-                                 where i > 0 && i % 2 == 0
-                                 select i;
+            //task2
 
-            Console.WriteLine("Positive and Equal");
-            foreach (var i in positiveEqual)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-
-            //task 4
-            var min = (from x in numbers select x).Min();
-            var max = (from x in numbers select x).Max();
-            var sum = (from x in numbers select x).Sum();
-
-            Console.WriteLine("Min {0} Max {1} Sum {2}", min, max, sum);
-           
-            //task 5
-            var average = (from x in numbers select x).Average();
-            var first = (from x in numbers where x < average select x).First();
-            Console.WriteLine("Average {0} First {1}", average, first);
-
-            //task 6
-            var sort = from x in numbers
-                       orderby x
-                       select x;
-
-            Console.WriteLine("Sorted");
-            foreach (var i in sort)
-            {
-                Console.Write(i + " ");
-            }
 
             Console.ReadKey();
-
+        }
+        static public double DoubleResult(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (a / b);
         }
     }
 }
