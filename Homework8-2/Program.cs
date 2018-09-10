25b6455503fb4b93ac22860b6aeb30b491760aab
Vasyl <w.kyrychuk@gmail.com>
2018-09-10 20:10:35 +0300

    Do

--- Homework8-2/Program.cs
+++ Homework8-2/Program.cs
@@ -32,29 +32,12 @@ namespace Homework8_2
                 count++;
                 Console.WriteLine("Line №{0} has {1} symbols", count, s.Length);
             }
-            int countMax = 0, countMin = 0;
-            string shortest = code[0], longest = code[0];
-            count = 0;
-            foreach (string s in code)
-            {
-                count++;
-                if (s.Length != 0)
-                {
-                    if (shortest.Length >= s.Length)
-                    {
-                        shortest = s;
-                        countMin = count;
-                    }
-                }
-                if (longest.Length <= s.Length)
-                {
-                    longest = s;
-                    countMax = count;
-                }
 
-            }
-            Console.WriteLine("The shortest line №{0} has {1} symbols\n{2}\nThe longest line №{3} has {4} symbols\n{5}",
-                countMin, shortest.Length, shortest, countMax, longest.Length, longest);
+            var max = code.Select(s => s.Length).Max();
+            var min = code.Select(s => s.Length).Min();
+
+            Console.WriteLine("The longest line - {0}, has {1} symbols", code.FirstOrDefault(s => s.Length == max),max);
+            Console.WriteLine("The shortest line - {0}, has {1} symbols", code.FirstOrDefault(s => s.Length == min),min);
 
             Console.WriteLine("\nLines with \"var\"");
 
