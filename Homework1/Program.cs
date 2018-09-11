4b691d66e20299f7ab08d2bcadc93613cda41692
Vasyl <w.kyrychuk@gmail.com>
2018-09-11 17:13:08 +0300

    9

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,28 +4,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*Develop interface IFlyable with method Fly().
-Create two classes Bird (with fields: name and canFly) and Plane  (with fields: mark and highFly),
-which implement interface IFlyable.
-Create List of IFlyable objects and add some Birds and Planes to it. 
-Call Fly() method for every item from the list of it*/
+/*Create struct Point:
+    - fields x and y, 
+    - method Distance() to calculate distance between points
+    - method ToString(), which return the Point in format "(x,y)"
+   Create class Triangle:
+    - fields vertex1, vertex2, vertex3 of type Point
+    - constructors
+    - methods Perimeter(), Square(), Print()
+  In the Main() create list of 3 triangles and write into console the information about these shapes.
+  *Print the triangle with vertex which is the closest to origin (0,0)
+*/
 
-namespace Homework5
+namespace Homework9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<IFlyable> flyes = new List<IFlyable>(3);
-            flyes.Add(new Bird("Owl", true));
-            flyes.Add(new Plane("Boing", 11000));
-            foreach (IFlyable f in flyes)
+            Point[] points = new Point[9];
+            Point origin = new Point(0, 0);
+            int j = 0;
+            for (int t = 0; t < 3; t++)
             {
-                f.Fly();
+                Console.WriteLine("Enter points of {0} triangle", t + 1);
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Enter x and y of {0} point", i + 1);
+                    points[i + j] = new Point(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+
+                }
+                j = j + 3;
             }
 
-            Console.ReadKey();
 
+            List<Triangle> triangles = new List<Triangle>();
+            triangles.Add(new Triangle(points[0], points[1], points[2]));
+            triangles.Add(new Triangle(points[3], points[4], points[5]));
+            triangles.Add(new Triangle(points[6], points[7], points[8]));
+            foreach (Triangle t in triangles)
+            {
+
+                Console.WriteLine("Perimeter - {0:F3}, Square - {1:F3}", t.Perimeter(), t.Square());
+                t.Print();
+            }
+            double shortest = Int32.MaxValue;
+            int count = 0, minCount = 0;
+            j = 0;
+            foreach (Triangle t in triangles)
+            {
+                double[] mins = new double[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    mins[i] = origin.Distance(points[i + j], origin);
+                }
+                j = j + 3;
+                count++;
+                if (shortest > mins.Min())
+                {
+                    shortest = mins.Min();
+                    minCount = count;
+                }
+
+            }
+            Console.WriteLine("Triangle №{0} is the closest to origin", minCount);
+            Console.ReadKey();
         }
     }
 }
---
+++ Homework1/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9
+{
+    public struct Point
+    {
+        public int x, y;
+        public Point(int X, int Y) { x = X; y = Y; }
+        public double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+        }
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", x, y);
+        }
+    }
+    public class Triangle
+    {
+        private Point vertex1, vertex2, vertex3;
+        public Triangle(Point a, Point b, Point c)
+        { vertex1 = a; vertex2 = b; vertex3 = c; }
+
+        public double Perimeter()
+        {
+            return vertex1.Distance(vertex1, vertex2) + vertex1.Distance(vertex1, vertex3) + vertex1.Distance(vertex2, vertex3);
+        }
+        public double Square()
+        {
+            return Math.Sqrt(0.5 * this.Perimeter() * (0.5 * this.Perimeter() - vertex1.Distance(vertex1, vertex2)) * (0.5 * this.Perimeter() - vertex1.Distance(vertex1, vertex3)) * (0.5 * this.Perimeter() - vertex1.Distance(vertex2, vertex3)));
+        }
+        public void Print()
+        {
+            Console.WriteLine(string.Format("Point 1 - {0}, point 2 - {1}, point 3 - {2}", vertex1.ToString(), vertex2.ToString(), vertex3.ToString()));
+        }
+
+    }
+}
